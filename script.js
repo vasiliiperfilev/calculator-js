011499428f0a71c9ee439ec2e8adf0a76fe8a62b
Vasilii Perfilev <vasily.perfilyev@gmail.com>
2021-11-24 22:09:47 -0800

    Add markup and styles

---
+++ script.js
@@ -0,0 +1,24 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function substract(num1, num2){
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2){
+    if (num2 == 0){
+        return "Zero division";
+    }
+    else {
+        return num1 / num2;
+    }
+}
+
+function operate(operator, num1, num2){
+    return operator(num1, num2);
+}
